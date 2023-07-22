6a52553e3e4681454b1572640b3e002b6d8a84d3
Kumaresan Vengadajalam <106003423+kumaka6@users.noreply.github.com>
2023-07-22 22:27:10 +0530

    Add files via upload

---
+++ MySQL_class/day1.sql
@@ -0,0 +1,4 @@
+create table testdb1.ineuron( emp_id int(10), emp_name varchar(15), emp_email varchar(20), emp_salary int(8));
+select *from testdb1.ineuron;
+select *from testdb1.ineuron1
+show databases
